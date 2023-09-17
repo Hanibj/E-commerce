513b5165ef5a4167fc41d10379e01f2e5d5d6973
hani ben jemaa <93058368+Hanibj@users.noreply.github.com>
2023-09-17 16:47:26 +0200

    update

--- frontend/src/Components/Routess/Routess.jsx
+++ frontend/src/Components/Routess/Routess.jsx
@@ -5,6 +5,9 @@ import Signin from '../../Pages/Authentification/Signin'
 import SignUp from '../../Pages/Authentification/SignUp'
 import Principale from '../../Pages/Shop/Principale'
 import Card from '../../Pages/Shop/Card'
+import Frip from '../../Client/Frip'
+import Personaliser from '../../Client/Personaliser'
+import NewProduct from '../../Client/NewProduct'
 
 
 const Routess = () => {
@@ -17,6 +20,9 @@ const Routess = () => {
         <Route path='/Signup' element={<SignUp/>}/>
         <Route path='/Market' element={<Principale/>}/>
         <Route path='/Card' element={<Card/>}/>
+        <Route path='/Client/Frip' element={<Frip/>}/>
+        <Route path='/Client/Personalisier' element={<Personaliser/>}/>
+        <Route path='/Client/Nouveau' element={<NewProduct/>}></Route>
     </Routes>
     </>
   )
